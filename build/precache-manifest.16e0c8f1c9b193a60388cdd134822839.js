dbf274eb30a6c1cffe94b1a4c70fecb43af07e3b
Tyler Poole <tyler.poole1441@gmail.com>
2024-07-23 22:31:14 -0500

    updated by npm run build

---
+++ build/precache-manifest.16e0c8f1c9b193a60388cdd134822839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723226d9b4cc876f9dc8790ea74fb09e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b2526843981d9abf4bc",
+    "url": "/static/css/main.380dab49.chunk.css"
+  },
+  {
+    "revision": "d46bcc03a89ce82c9ffc",
+    "url": "/static/js/2.5ce518ae.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.5ce518ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2526843981d9abf4bc",
+    "url": "/static/js/main.6583a65e.chunk.js"
+  },
+  {
+    "revision": "b63828fdddc587413eb0",
+    "url": "/static/js/runtime-main.a5768cfb.js"
+  }
+]);
